9c99f72d4fcdb533535330221349af47dca3d388
1trickPwnyta <sarahpace37@gmail.com>
2024-08-13 18:58:00 -0700

    Add bed unassignment to breakups due to new relationships and rejected proposals

---
+++ 1.5/Source/Patch_InteractionWorker_RomanceAttempt.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace SensibleBedOwnership
+{
+    // Unassign one pawn from every bed they share on every map
+    [HarmonyPatch(typeof(InteractionWorker_RomanceAttempt))]
+    [HarmonyPatch("BreakLoverAndFianceRelations")]
+    public static class Patch_InteractionWorker_RomanceAttempt
+    {
+        public static void Postfix(Pawn pawn, List<Pawn> oldLoversAndFiances)
+        {
+            foreach (Pawn other in oldLoversAndFiances)
+            {
+                Utility.UnassignBedsAfterBreakup(pawn, other);
+            }
+        }
+    }
+}
