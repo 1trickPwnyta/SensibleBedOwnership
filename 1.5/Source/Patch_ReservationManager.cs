47c07e5004a333d9d624a913907425c41354392c
1trickPwnyta <sarahpace37@gmail.com>
2024-12-17 12:04:16 -0800

    Prevent double reservations in beds

---
+++ 1.5/Source/Patch_ReservationManager.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace SensibleBedOwnership
+{
+    [HarmonyPatch(typeof(ReservationManager))]
+    [HarmonyPatch(nameof(ReservationManager.Reserve))]
+    public static class Patch_ReservationManager
+    {
+        public static bool Prefix(Pawn claimant, LocalTargetInfo target, ref bool __result)
+        {
+            HashSet<Pawn> reservers = new HashSet<Pawn>();
+            if (target.HasThing && target.Thing is Building_Bed)
+            {
+                target.Thing.Map.reservationManager.ReserversOf(target, reservers);
+                if (reservers.Contains(claimant))
+                {
+                    __result = true;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
