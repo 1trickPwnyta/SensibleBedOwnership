95af87de68d34c2260b97743f326384afebc7d9f
1trickPwnyta <sarahpace37@gmail.com>
2024-12-17 13:15:23 -0800

    Allow pawns to sleep in chairs

--- 1.5/Source/SensibleBedOwnershipMod.cs
+++ 1.5/Source/SensibleBedOwnershipMod.cs
@@ -1,5 +1,6 @@
 using HarmonyLib;
 using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace SensibleBedOwnership
@@ -9,13 +10,25 @@ namespace SensibleBedOwnership
         public const string PACKAGE_ID = "sensiblebedownership.1trickPwnyta";
         public const string PACKAGE_NAME = "Sensible Bed Ownership";
 
+        public static SensibleBedOwnershipSettings Settings;
+
         public SensibleBedOwnershipMod(ModContentPack content) : base(content)
         {
             var harmony = new Harmony(PACKAGE_ID);
             harmony.PatchAll();
             harmony.Patch(typeof(Dialog_AssignBuildingOwner).Constructor(new[] { typeof(CompAssignableToPawn) }), null, typeof(Patch_Dialog_AssignBuildingOwner_ctor).Method("Postfix"));
 
+            Settings = GetSettings<SensibleBedOwnershipSettings>();
+
             Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+
+        public override string SettingsCategory() => PACKAGE_NAME;
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            SensibleBedOwnershipSettings.DoSettingsWindowContents(inRect);
         }
     }
 }
